2e5e5c45f83e6212d1031fe4c6237060edeab892
Malte <malte.schuette92@gmail.com>
2026-06-22 21:46:36 +0200

    Add media file serving to urlpatterns for static file handling

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('django-rq/', include('django_rq.urls')),
     path('api/', include('users_app.api.urls')),
     path('api/', include('videos_app.api.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
