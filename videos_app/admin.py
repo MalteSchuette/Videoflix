dbfc6225543679208b2e6247f7f112dbf6aaa3bb
Malte <malte.schuette92@gmail.com>
2026-06-22 23:44:05 +0200

    Refactor Video admin registration to use a custom form for optional video_file field

--- videos_app/admin.py
+++ videos_app/admin.py
@@ -1,4 +1,23 @@
+from django import forms
 from django.contrib import admin
 from .models import Video
 
-admin.site.register(Video)
+
+class VideoAdminForm(forms.ModelForm):
+    """Admin form that makes video_file optional when editing existing videos."""
+
+    class Meta:
+        model = Video
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['video_file'].required = False
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    """Admin configuration for the Video model."""
+
+    form = VideoAdminForm
